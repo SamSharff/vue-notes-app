0c14fe8f33f29d57a39fa0e1cdd6c9e8cda3e620
Sam Sharff <samanthaleec@gmail.com>
2022-04-19 17:02:15 -0400

    adds component solutions

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,16 +1,38 @@
+<script>
+import NoteSelector from "./NoteSelector.vue";
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is first note", timestamp: Date.now() },
+        { id: 2, body: "This is second note", timestamp: Date.now() },
+        { id: 3, body: "This is third note", timestamp: Date.now() },
+        { id: 4, body: "This is fourth note", timestamp: Date.now() },
+        { id: 5, body: "This is fifth note", timestamp: Date.now() },
+        { id: 6, body: "This is sixth note", timestamp: Date.now() },
+      ],
+      selectedNoteId: 1,
+    };
+  },
+  methods: {
+    selectNote: function (note) {
+      this.selectedNoteId = note.id;
+    },
+  },
+};
+</script>
+
 <template>
   <div class="note-selectors">
-    <div class="note-selector active">
-      <p class="note-selector-title">First note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
-    <div class="note-selector">
-      <p class="note-selector-title">Second note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
-    <div class="note-selector">
-      <p class="note-selector-title">Third note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
+    <NoteSelector
+      v-for="note in notes"
+      v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
+      v-on:selectNote="selectNote"
+      v-bind:key="note.id"
+    />
   </div>
 </template>
